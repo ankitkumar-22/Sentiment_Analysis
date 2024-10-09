38f738914a7e95d6441ee0bb873445229ea15fd4
ankitkumar-22 <121615440+ankitkumar-22@users.noreply.github.com>
2024-10-09 10:37:35 +0530

    Add files via upload

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import nltk
+plt.style.use('ggplot')
+import re
+from nltk.corpus import stopwords
+
+
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
+    text = re.sub(r'\@\w+|\#','', text)
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    tokens = text.split()
+    tokens = [word for word in tokens if word not in stopwords.words('english')]
+    return ' '.join(tokens)
+
+def RobertA(sentence):
+    sentence = clean_text(sentence)
+    encoded_text = tokenizer(sentence, return_tensors='pt')
+    output = model(**encoded_text)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    scores_dict = {
+        'Negative' : scores[0],
+        'Neutral' : scores[1],
+        'Positive' : scores[2]
+    }
+    return (scores_dict)
+
+sentence = "The Germans killed the millions of jews in concentration camps"
+scores = RobertA(sentence)
+print(scores)
+
+#%%
